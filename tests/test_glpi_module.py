5d12068550a30d7d806231d341f70851fa0012f5
Marco Tulio R Braga <marco.tulio@mtulio.eng.br>
2017-04-17 03:09:27 -0300

    [enhancement] Add Get/create to one generic Item object

--- tests/test_glpi_module.py
+++ tests/test_glpi_module.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv, find_dotenv
 from glpi import GlpiProfile
 from glpi import GlpiTicket, Ticket
 from glpi import GlpiKnowBase, KnowBase
+from glpi import GLPI
 
 load_dotenv(find_dotenv())
 
@@ -38,7 +39,7 @@ def test_ticket():
                              username=username,
                              password=password)
 
-    print "Update Ticket object to session: %s" %\
+    print "##> Update Ticket object to session: %s" %\
             glpi_ticket.update_session_token(token_session)
 
     tickets_all = glpi_ticket.get_all()
@@ -49,8 +50,8 @@ def test_ticket():
 
     ticket = Ticket(name="New ticket from SDK %s" % t,
                     content=" Content of ticket created by SDK API  %s" % t)
-    ticket_dict = glpi_ticket.create(ticket)
-    print "Created the ticket polymorphism: %s" % ticket_dict
+    ticket_dict = glpi_ticket.create(ticket.get_data())
+    print "Created the ticket: %s" % ticket_dict
 
     print "Getting ticket recently created with id %d ..." % ticket_dict['id']
     ticket_get = glpi_ticket.get(ticket_dict['id'])
@@ -105,6 +106,75 @@ def test_kb():
     print "Creating: %s " % kb_dict
 
 
+def test_general():
+
+    # Basic usage
+    glpi = GLPI(url, glpi_app_token, (username, password))
+
+    print "#> Getting help()"
+    print glpi.help_item()
+
+    print "#> Getting standard items: ticket"
+    print json.dumps(glpi.get_all('ticket'),
+                      indent=4,
+                      separators=(',', ': '),
+                      sort_keys=True)
+
+    print "#> Setting up new items..."
+    new_map = {
+        "ticket": "/Ticket",
+        "knowbase": "/knowbaseitem",
+        "problem": "/problem",
+        "change": "/change",
+        "computer": "/computer",
+        "software": "/software",
+        "network": "/networkequipment",
+    }
+    glpi.set_item_map(new_map)
+    print "#> Getting new item: COMPUTER"
+    print json.dumps(glpi.get_all('computer'),
+                      indent=4,
+                      separators=(',', ': '),
+                      sort_keys=True)
+
+    # Setting up new map
+    print "#####> Creating new MAP..."
+    new_map = {
+         "knowbase": "/knowbaseitem"
+    }
+    glpi2 = GLPI(url, glpi_app_token, (username, password),
+                 item_map=new_map)
+
+    print "#> Getting item: KB"
+    print json.dumps(glpi2.get_all('knowbase'),
+                       indent=4,
+                       separators=(',', ': '),
+                       sort_keys=True)
+
+    print "#> Getting item KB by ID 1"
+    kb_dict = glpi2.get('knowbase', 1)
+    print json.dumps(kb_dict,
+                       indent=4,
+                       separators=(',', ': '),
+                       sort_keys=True)
+    print kb_dict
+    print "#> Creating new KB copying from previous..."
+    kb_data = {
+        "name": "New KB copyied from ID %s at %s" % (
+                                        kb_dict['id'], t),
+        "answer": "Description of KB: \n <br> API Desc </br> Just a test ",
+        "is_faq": kb_dict['is_faq'],
+        "knowbaseitemcategories_id": kb_dict['knowbaseitemcategories_id'],
+        "users_id": kb_dict['users_id'],
+        "view": kb_dict['view']
+    }
+    print "Creating object data: %s" % repr(kb_data)
+    kb_res = glpi2.create('knowbase', kb_data)
+    print json.dumps(kb_res,
+                     indent=4,
+                     separators=(',', ': '),
+                     sort_keys=True)
+
 if __name__ == '__main__':
 
     vcap_service_credentials = load_from_vcap_services('glpi')
@@ -124,6 +194,7 @@ if __name__ == '__main__':
     print repr(vcap_service_credentials)
     t = time.strftime("%Y/%m/%d-%H:%M:%S")
 
-    test_profile()
+    # test_profile()
     test_ticket()
-    test_kb()
+    # test_kb()
+    test_general()
